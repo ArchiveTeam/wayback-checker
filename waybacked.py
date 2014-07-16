bb48c01e7860493e993e273e1a94bc8ad9c910cb
Ivan Kozik <ivan@ludios.org>
2014-07-16 01:28:45 +0000

    Use JSON API, so that we can check if non-HTML resources are there without grabbing them

--- waybacked.py
+++ waybacked.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
 import sys
+import json
+import urllib
 import requests
 
 def is_on_wayback(url):
-	response = requests.head("http://web.archive.org/web/*/" + url)
-	if response.status_code == 200:
-		return True
-	elif response.status_code == 404:
-		return False
-	else:
-		raise ValueError("Unexpected response code %r for %r" % (response.status_code, url))
+	response = requests.get("http://archive.org/wayback/available?url=" + urllib.quote(url))
+	data = json.loads(response.content)
+	return bool(data["archived_snapshots"])
 
 def main():
 	for line in sys.stdin:
 		url = line.rstrip()
+		if not url:
+			continue
 		if not is_on_wayback(url):
 			print "!ao %s" % (url,)
 			sys.stdout.flush()
